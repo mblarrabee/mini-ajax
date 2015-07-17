536205610eaa96bc5aec52827792a7b201ab519f
mblarrabee <mblarrabee421@gmail.com>
2015-07-16 21:09:26 -0600

    did using append method instead of html method

---
+++ app.js
@@ -0,0 +1,56 @@
+$(document).ready(function (){
+	$("#getUsers").on("click", function() {
+		return $.ajax({
+			method: "GET",
+			url: 'http://reqr.es/api/users?page=1',
+			success: function(res) {
+				console.log(res);
+				insertData(res.data);
+			}
+		})
+	})
+
+	$('#addUser').on('click', function(e) {
+    e.preventDefault();
+    var userName = $('#name').val();
+    var userJob = $('#job').val();
+    return $.ajax({
+      method: 'POST',
+      url: 'http://reqr.es/api/users',
+      data: {name: userName, job: userJob},
+      success: function(res) {
+        $('#recentUser').html(
+          '<li>' +
+            'name: ' + res.name +
+          '</li>' +
+          '<li>' +
+            'job: ' + res.job +
+          '</li>' +
+          '<li>' +
+            'id: ' + res.id +
+          '</li>' +
+          '<li>' +
+            'created at: ' + res.createdAt +
+          '</li>'
+        )
+      }
+    })
+  });
+	
+})
+
+function insertData(arr){
+	for(var i = 0; i < arr.length; i++){
+		$(".col-md-6 > .well").append("<div>" +
+			"User Info:" +
+			"<li>" +
+			"First name: " + arr[i].first_name +
+			"</li>" +
+			"<li>" +
+			"Last name: " + arr[i].last_name +
+			"</li" +
+			"<hr>" +
+			"</div>"
+		)
+	};
+}
